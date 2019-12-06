6143a22a3928a185047d4e421e4939bb6bb5fe6b
DylanPJackson <frprdjackson@gmail.com>
2019-12-06 01:36:48 -0500

    Updates for 3rd star

--- 2_day/3_star.R
+++ 2_day/3_star.R
@@ -1,2 +1,38 @@
 ## Read in the data
 input <- read.csv("input.csv", header=FALSE, sep=",")
+
+## Create function to parse intcode
+intcode <- function(input){
+    # Iterate through eac element in vector
+    for(i in 1:129){
+        # Only check every four elements for an opcode
+        if(i %% 4 == 1){
+            # Determine which opcode it is
+            op <- input[[i]]
+            if(op == 1){
+                # Add 1 to each index because R vectors start at 1
+                p_1 <- input[[i+1]] + 1
+                p_2 <- input[[i+2]] + 1
+                index <- input[[i+3]] + 1
+                # Calculate result with given indices and store
+                result <- input[[p_1]] + input[[p_2]]
+                input[[index]] <- result
+            } else if(op == 2){
+                # Add 1 to each index because R vectors start at 1
+                p_1 <- input[[i+1]] + 1
+                p_2 <- input[[i+2]] + 1
+                index <- input[[i+3]] + 1
+                # Calculate result with given indices and store
+                result <- input[[p_1]] * input[[p_2]]
+                input[[index]] <- result
+            } else if(op == 99){
+                # Return the entire vector when terminate
+                return(input)
+            } else {
+                return ("Broken probably")
+            }
+        }
+    }    
+}
+
+intcode(input)[[1]]
--- 2_day/README.md
+++ 2_day/README.md
@@ -2,3 +2,9 @@
 You can't read in a comma separated file the same way you'd read in a newline
 separated file. I had save the input in a .csv file rather than a .txt file
 this time, and read it in using a different function with different parameters
+1. Comma separated files are much different from .txt
+2. When dealing with input that assumes 0 indexed, arrays, you do need to
+compensate for R
+3. A bit more about indexing and vectors
+4. You can actually edit a function after creating it in the interpreter using
+the edit function
